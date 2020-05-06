5b76736d8a1b850b4b59f028d41b65e66ae0ac63
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-05-06 03:02:03 -0700

    optional test route, some basic logging

--- app.js
+++ app.js
@@ -8,18 +8,27 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('combined'));
+app.use(morgan('combined', {
+	stream: fs.createWriteStream('server.log', {flags: 'a'})
+}));
 
-const productUrl = 'https://www.costco.com/nintendo-switch-bundle-with-12-month-online-family-plan-and-case.product.100519747.html';
+let productUrl = 'https://www.costco.com/nintendo-switch-bundle-with-12-month-online-family-plan-and-case.product.100519747.html';
+const testUrl = 'https://www.costco.com/springfield-club-chair.product.11659957.html'; // product that was in stock at time of testing, for testing purposes.
 const inStockString = '<img class="oos-overlay hide" src="/wcsstore/CostcoGLOBALSAS/images/OOS-overlay-en.png" alt="Out of Stock" title="Out of Stock"/>'; // a string only found on the page when it IS in stock
 
 app.get('/', (req, res) => {
-	res.send(`Server up and running.`);
+	let html = '<p>App up and running, go ';
+	html += '<a href="/checkcostco">here</a>'
+	html += ' to run <strong>Costco Switch Checker</strong>.'
+	res.send(html);
 });
 
-app.get('/checkcostco', (req, res) => {
-	
-	// const url = 'https://www.costco.com/springfield-club-chair.product.11659957.html'; // product that was in stock at time of testing, for testing purposes.
+app.get('/checkcostco/:test?', (req, res) => {
+	if (req.params.test) {
+		console.log(`Running test request against product that should be in stock.`);
+		productUrl = testUrl;
+	}
+
 	axios.get(productUrl)
 		.then((response) => {
 			console.log(`Request happened sucessfully to ${productUrl}`);
@@ -31,13 +40,13 @@ app.get('/checkcostco', (req, res) => {
 				res.send('Looks like the Nintendo Switch is back in stock!') :
 				res.send(`Alas, still out of stock :( @ ${now.toLocaleString()}`);
 
-			fs.writeFile('log.html', response.data, function(err) {
+			fs.writeFile('costco-response.html', response.data, function(err) {
 				if (err) { throw err }
 
 				console.log('Response from Costco website written to log.html file.');
 			});
 		}).catch((err) => {
-			console.error(`Uh-oh, looks like there was an error. Error code: ${err}`);
+			console.error(`Uh-oh, looks like there was an error. Message: ${err}`);
 		});
 });
 
