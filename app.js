fcd0f71990ef9344efc1e2264672e7e67830fd7d
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-05-06 03:29:58 -0700

    403d from setInterval

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan('combined', {
-	stream: fs.createWriteStream('server.log', {flags: 'a'})
+	stream: fs.createWriteStream('server.log', { flags: 'a' })
 }));
 
 let productUrl = 'https://www.costco.com/nintendo-switch-bundle-with-12-month-online-family-plan-and-case.product.100519747.html';
@@ -36,18 +36,33 @@ app.get('/checkcostco/:test?', (req, res) => {
 
 			const now = new Date();
 
-			response.data.includes(inStockString) ? 
+			response.data.includes(inStockString) ?
 				res.send('Looks like the Nintendo Switch is back in stock!') :
 				res.send(`Alas, still out of stock :( @ ${now.toLocaleString()}`);
 
-			fs.writeFile('costco-response.html', response.data, function(err) {
+			fs.writeFile('costco-response.html', response.data, function (err) {
 				if (err) { throw err }
 
-				console.log('Response from Costco website written to log.html file.');
+				console.log('Response from Costco website written to costco-response.html file.');
 			});
 		}).catch((err) => {
 			console.error(`Uh-oh, looks like there was an error. Message: ${err}`);
 		});
+
+	// lol instant 403 response when I ran this
+	// const makeRequest = () => {
+	// 	axios.get(`http://localhost:${PORT}/checkcostco`)
+	// 		.then((response) => {
+	// 			console.log('Request itself');
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(`Didn't like that! Error: ${err}`);
+	// 		});
+	// };
+
+	// setInterval(makeRequest, 5000);
+
+
 });
 
 app.listen(PORT, () => {
